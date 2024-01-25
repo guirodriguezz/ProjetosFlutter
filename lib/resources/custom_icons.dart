52dac75aba5a894252696d238684da7fa7931594
Guilherme Alexandre Marques <gui.rodrigues@msn.com>
2024-01-25 19:02:18 -0300

    Adicionando custom icons no projeto. Adicionando também TabBar.

---
+++ lib/resources/custom_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData baseball_ball = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData american_football = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData football = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
--- lib/sports_gym.dart
+++ lib/sports_gym.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ginasio_esportes_flutter/resources/custom_icons.dart';
 import 'package:ginasio_esportes_flutter/pages/calendar_page.dart';
 import 'package:ginasio_esportes_flutter/pages/profile_page.dart';
 import 'package:ginasio_esportes_flutter/pages/ranking_page.dart';
@@ -20,6 +21,11 @@ class SportsGym extends StatefulWidget {
 class _SportsGymState extends State<SportsGym> {
   final pageController = PageController();
   int currentPage = 0;
+  final tabs = const [
+    Tab(icon: Icon(CustomIcons.baseball_ball)),
+    Tab(icon: Icon(CustomIcons.football)),
+    Tab(icon: Icon(CustomIcons.american_football)),
+  ];
 
   void onItemPressed(int page) {
     pageController.jumpToPage(page);
@@ -38,69 +44,79 @@ class _SportsGymState extends State<SportsGym> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: theme.colorScheme.inversePrimary,
-        elevation: 5,
-        shadowColor: Colors.black,
-        title: const Text(Strings.appName),
-        actions: [
-          IconButton(
-            onPressed: widget.onThemeModePressed,
-            icon: Icon(
-              theme.brightness == Brightness.light
-                  ? Icons.dark_mode
-                  : Icons.light_mode,
-            ),
-          ),
-        ],
-      ),
-      body: PageView(
-        controller: pageController,
-        onPageChanged: onPageChanged,
-        children: const [
-          SportsPage(),
-          RankingPage(),
-          CalendarPage(),
-          ProfilePage(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        onPressed: () {},
-        child: const Icon(Icons.share),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        elevation: 10,
-        notchMargin: 7,
-        shape: const CircularNotchedRectangle(),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          iconSize: 22,
-          showUnselectedLabels: false,
-          currentIndex: currentPage,
-          onTap: onItemPressed,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.sports),
-              label: Strings.sports,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: Strings.ranking,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today),
-              label: Strings.calendar,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: Strings.profile,
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: theme.colorScheme.inversePrimary,
+          elevation: 5,
+          shadowColor: Colors.black,
+          title: const Text(Strings.appName),
+          actions: [
+            IconButton(
+              onPressed: widget.onThemeModePressed,
+              icon: Icon(
+                theme.brightness == Brightness.light
+                    ? Icons.dark_mode
+                    : Icons.light_mode,
+              ),
             ),
           ],
+          bottom: currentPage == 0
+              ? TabBar(
+                  tabs: tabs,
+                  indicatorPadding: const EdgeInsets.symmetric(horizontal: -30),
+                )
+              : null,
+        ),
+        body: PageView(
+          controller: pageController,
+          onPageChanged: onPageChanged,
+          children: const [
+            SportsPage(),
+            RankingPage(),
+            CalendarPage(),
+            ProfilePage(),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          onPressed: () {},
+          child: const Icon(Icons.share),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          elevation: 10,
+          notchMargin: 7,
+          shape: const CircularNotchedRectangle(),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            iconSize: 22,
+            showUnselectedLabels: false,
+            currentIndex: currentPage,
+            onTap: onItemPressed,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.sports),
+                label: Strings.sports,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: Strings.ranking,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today),
+                label: Strings.calendar,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: Strings.profile,
+              ),
+            ],
+          ),
         ),
       ),
     );
